37bfec6d0ec45572dc96b5edec0e4c73bb6f01fb
Mansi Ganar <86571822+MansiGanar@users.noreply.github.com>
2021-09-28 15:39:09 +0200

    Layout

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h2>
+        <i className="fas fa-book fa-lg"> Books</i>
+      </h2>
+    </header>
+  );
+};
+
+export default Header;
